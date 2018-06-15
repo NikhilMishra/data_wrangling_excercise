412832a52c94daa1ea8f56fc42644dbefc1a2280
NikhilMishra <mishranikh@gmail.com>
2018-06-15 11:13:53 -0700

    plots the average life expectancy of continents from 1952 to 2007

---
+++ life_expectancy.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(gapminder)
+#filter all data into datasets by continent and change column names to reflect respective continents
+asia <- gapminder %>% 
+  filter(continent == "Asia") %>% 
+  group_by(year) %>%
+  summarize(mean_exp = mean(lifeExp))
+colnames(asia) <- c("year", "asiaLife")
+americas <- gapminder %>% 
+  filter(continent == "Americas") %>% 
+  group_by(year) %>%
+  summarize(mean_exp = mean(lifeExp))
+colnames(americas) <- c("year", "americasLife")
+europe <- gapminder %>% 
+  filter(continent == "Europe") %>% 
+  group_by(year) %>%
+  summarize(mean_exp = mean(lifeExp))
+colnames(europe) <- c("year", "europeLife")
+africa <- gapminder %>% 
+  filter(continent == "Africa") %>% 
+  group_by(year) %>%
+  summarize(mean_exp = mean(lifeExp))
+colnames(africa) <- c("year", "africaLife")
+oceania <- gapminder %>% 
+  filter(continent == "Oceania") %>% 
+  group_by(year) %>%
+  summarize(mean_exp = mean(lifeExp))
+colnames(oceania) <- c("year", "oceaniaLife")
+#merge datasets to create a dataset with mean life expectancy from each continent
+total <- merge(asia,americas, by = "year")
+total <- merge(total,europe, by = "year")
+total <- merge(total,africa, by = "year")
+total <- merge(total,oceania, by = "year")
+#center title
+theme_update(plot.title = element_text(hjust = 0.5))
+#plot graph
+ggplot(total, aes(x = year)) + 
+  geom_line(aes(y = asiaLife, color = 'Asia')) +
+  geom_line(aes(y = americasLife, color = 'Americas')) +
+  geom_line(aes(y = africaLife, color = 'Africa')) +
+  geom_line(aes(y = europeLife, color = 'Europe')) +
+  geom_line(aes(y = oceaniaLife, color = 'Oceania')) + 
+  labs(y="Average Lifespan (years)", x = "Date", color = "Continent") +
+  ggtitle("Change in Lifespan From 1952 to 2007") + 
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9", "#A00000", "#FF1234"))
